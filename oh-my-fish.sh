990d0f0984b4fb23a55bbbbd55bea3aa81474d6a
CacheSysop <stephenhall1981@gmail.com>
2026-05-25 04:41:18 -0700

    Update oh-my-fish.sh

--- oh-my-fish.sh
+++ oh-my-fish.sh
@@ -3,7 +3,7 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install
 
 omf install lambda
-omf theme lambda
+
