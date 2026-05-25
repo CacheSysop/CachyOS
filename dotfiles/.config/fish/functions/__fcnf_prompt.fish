2a79b0170850f2735b1b5d979f15b63d33b37d4f
CacheSysop <stephenhall1981@gmail.com>
2026-05-25 03:29:22 -0700

    omf fish config

---
+++ dotfiles/.config/fish/functions/__fcnf_prompt.fish
@@ -0,0 +1,8 @@
+function __fcnf_prompt --argument-names layout pkg
+    switch $layout
+        case minimal
+            echo (set_color --bold blue)"::"(set_color normal)" "(set_color --bold)(__fcnf_i18n prompt_minimal)(set_color normal)
+        case '*'
+            echo (set_color --bold blue)"::"(set_color normal)" "(set_color --bold)(__fcnf_i18n prompt_single)(set_color normal)
+    end
+end
